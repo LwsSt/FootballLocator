8dee8a55585094fd4689c35c0676c96d9b86b217
Lewis Stapleton <lewis.stapleton@outlook.com>
2025-10-07 19:55:27 +0100

    Include uid in iCAL data

--- src/lib/ts-modules/ical.d.ts
+++ src/lib/ts-modules/ical.d.ts
@@ -2,6 +2,7 @@ declare module 'ical' {
     declare function parseICS(input:string):Record<string, ICalData>;
 
     declare interface ICalData {
+        uid:        string;
         summary:    string;
         location:   string;
         start:      Date;
