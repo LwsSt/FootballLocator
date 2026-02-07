4a45f4345fe0dfa9a42af8d8cc93c741c3e7f2ea
Lewis Stapleton <lewis.stapleton@outlook.com>
2026-02-07 18:42:53 +0000

    Remove custom Date types

--- src/lib/matchData.ts
+++ src/lib/matchData.ts
@@ -1,5 +1,5 @@
 import ICAL from 'ical.js';
-import { MatchData, RawMatchData, Location, Day, Time } from "./types";
+import { MatchData, Location } from "./types";
 import { Stadia } from "./stadia";
 
 const parseTeams = (summary:string):[string, string]|null => {
@@ -13,17 +13,22 @@ const parseTeams = (summary:string):[string, string]|null => {
   }
 }
 
-const parseMatchData = (icalData:ICAL.Event[]):RawMatchData[] => {
+const getStadium = (name:string):Location => {
+  return Stadia.getInstance().getStadium(name);
+}
+
+const parseMatchData = (icalData:ICAL.Event[]):MatchData[] => {
   return icalData.map(i => {
     const teams = parseTeams(i.summary);
+    const location = getStadium(i.location);
 
     if (teams) {
       return {
         uid: i.uid,
         teams: teams,
-        location: i.location,
-        endDate: i.endDate.toJSDate(),
-        startDate: i.startDate.toJSDate(),
+        location: location,
+        end: i.endDate.toJSDate(),
+        start: i.startDate.toJSDate(),
       };
     } else {
       return null;
@@ -32,54 +37,9 @@ const parseMatchData = (icalData:ICAL.Event[]):RawMatchData[] => {
   .filter(i => i != null);
 }
 
-const getStadium = (name:string):Location => {
-  return Stadia.getInstance().getStadium(name);
-}
-
-const getTimeInfo = (date:Date):Time => {
-  return new Time(
-    date.getHours(),
-    date.getMinutes(),
-  );
-}
-
-const getDayAndTimeInfo = (rawData:RawMatchData):{day:Day, startTime:Time, endTime:Time} => {
-  const day:Day = new Day(
-    rawData.startDate.getFullYear(),
-    rawData.startDate.getMonth(),
-    rawData.startDate.getDay()
-  );
-
-  const startTime:Time = getTimeInfo(rawData.startDate);
-  const endTime:Time = getTimeInfo(rawData.endDate);
-
-  return {
-    day,
-    startTime,
-    endTime
-  }
-}
-
-const enrichMatchData = (rawData:RawMatchData):MatchData => {
-  const location = getStadium(rawData.location);
-
-  const { day, startTime, endTime } = getDayAndTimeInfo(rawData);
-
-  return {
-    teams: rawData.teams,
-    uid: rawData.uid,
-    day: day,
-    startTime: startTime,
-    endTime: endTime,
-    location: location,
-  }
-}
 
 export const getMatchData = (icalData:ICAL.Event[]) : MatchData[] => {
-  const rawMatchData = parseMatchData(icalData);
-  const matchData = rawMatchData.map(enrichMatchData)
-    .sort((a, b) => a.day.compare(b.day))
-  ;
+  const matchData = parseMatchData(icalData);
 
   return matchData;
 }
